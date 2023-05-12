e686cac19d2263ac69f0dd9a107fca4511b06676
davidktlee <rudxor567@naver.com>
2023-05-12 21:46:27 +0900

    필요없는 파일 삭제

--- udemy-lecture/App.js
+++
@@ -1,8 +0,0 @@
-import { StatusBar } from 'expo-status-bar'
-import { FlatList, StyleSheet, Text, View } from 'react-native'
-import { CATEGORIES } from './data/dummy'
-import { CategoriesScreen } from './screen/CategoriesScreen'
-
-export default function App() {
-  return <CategoriesScreen />
-}
